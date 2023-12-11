565c753b5c022af125ff803b13925879a7c33864
Jelle Adyns <137185777+JelleAdyns@users.noreply.github.com>
2023-12-11 21:51:26 +0100

    Initialized DirectX

--- source/Renderer.h
+++ source/Renderer.h
@@ -21,6 +21,13 @@ namespace dae
 
 	private:
 		SDL_Window* m_pWindow{};
+		ID3D11Device* m_pDevice{};
+		ID3D11DeviceContext* m_pDeviceContext{};
+		IDXGISwapChain* m_pSwapChain{};
+		ID3D11Texture2D* m_pDepthStencilBuffer{};
+		ID3D11DepthStencilView* m_pDepthStencilView{};
+		ID3D11Resource* m_pRenderTargetBuffer{};
+		ID3D11RenderTargetView* m_pRenderTargetView{};
 
 		int m_Width{};
 		int m_Height{};
